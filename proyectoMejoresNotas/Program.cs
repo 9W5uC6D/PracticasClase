c852be741e6d93c113b9a271a2c7661bdb120a93
Aaron <167219612+9W5uC6D@users.noreply.github.com>
2025-12-01 17:04:59 -0400

    Add proyectoMejoresNotas project and refactor Program.cs
    Added a new project 'proyectoMejoresNotas' with logic to input student names, generate random grades, and calculate averages based on the best grades. Refactored 'proyectoNotasEstudiantes/Program.cs' to improve formatting, streamline methods, and fix minor logic issues for better readability and maintainability.

---
+++ proyectoMejoresNotas/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace proyectoMejoresNotas;
+
+class Alumno
+{
+    public string Nombre { set; get; }
+    public List<int> ListaDeNotas { set; get; }
+    public int NotaFinal { set; get; }
+
+    public Alumno(string nombre)
+    {
+        Nombre = nombre;
+        ListaDeNotas = new List<int>();
+        NotaFinal = 0;
+    }
+}
+class Program
+{
+    private static Random r;
+    private static int objsCreados;
+    static void Main(string[] args)
+    {
+        List<Alumno> listaAlumnos = new List<Alumno>();
+        creacionObj();
+        addNombreEstudiantes(listaAlumnos);
+        //mostrarNombres(listaAlumnos);
+        generarNotaParaLosAlumnos(listaAlumnos);
+        mostrarNotasAlumnos(listaAlumnos);
+        mejoresNotas(listaAlumnos);
+
+    }
+    private static void creacionObj()
+    {
+        objsCreados++;
+        if (objsCreados == 1) r = new Random();
+    }
+
+    private static void addNombreEstudiantes(List<Alumno> listaAlumnos)
+    {
+        int i = 1;
+        bool finalizar = true;
+        string nombreAlumno = string.Empty;
+        do
+        {
+            Console.WriteLine($"\nIngresa el nombre del Alumno N° {i++}");
+            Console.Write("Nombre: ");
+            listaAlumnos.Add(new Alumno(Console.ReadLine()));
+            Console.Write($"\nAgregar a otro alumno? (s/n) : ");
+            nombreAlumno = Console.ReadLine();
+            finalizar = nombreAlumno == "S" || nombreAlumno == "s" ? true : false;
+        } while (finalizar);
+    }
+    private static void generarNotaParaLosAlumnos(List<Alumno> listadeAlumnos)
+    {
+        Console.Write("Ingrese la cantidad de notas a generar: ");
+        int cantidadDeNotas = Convert.ToInt32(Console.ReadLine());
+        foreach (var alumno in listadeAlumnos)
+        {
+            for (int i = 0; i < cantidadDeNotas; i++)
+            {
+                alumno.ListaDeNotas.Add(generarNota());
+            }
+        }
+    }
+    private static void mostrarNotasAlumnos(List<Alumno> listadeAlumnos)
+    {
+        Console.WriteLine("\n\n--- NOTAS DE LOS ALUMNOS ---");
+        Console.WriteLine("----------------------------------");
+        foreach (Alumno alumno in listadeAlumnos)
+        {
+            Console.WriteLine($"{alumno.Nombre}\t\t");
+            Console.WriteLine(string.Join(", ", alumno.ListaDeNotas));
+            if (alumno.NotaFinal != 0)
+            {
+                Console.WriteLine($"Promedio: {alumno.NotaFinal}\n");
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+    private static void mejoresNotas(List<Alumno> listadeAlumnos)
+    {
+        Console.Write("Ingrese la cantidad de mejores notas que quiera contar: ");
+        int cantidadDeNotas = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"\n\n--- MJORES {cantidadDeNotas} NOTAS DE LOS ALUMNOS ---\n");
+        foreach (var alumno in listadeAlumnos)
+        {
+            int suma = 0;
+            alumno.ListaDeNotas.Sort();
+            alumno.ListaDeNotas.Reverse();
+            Console.WriteLine($"{alumno.Nombre}");
+            for (int i = 0; i < cantidadDeNotas; i++)
+            {
+                Console.Write($"{alumno.ListaDeNotas[i]} ");
+                suma += alumno.ListaDeNotas[i];
+            }
+            double promedio = suma / cantidadDeNotas;
+            Console.WriteLine($"\nPromedio: {promedio:F2}");
+            if (promedio > 60)
+            {
+                Console.WriteLine("APROBADO\n");
+            }
+            else
+            {
+                Console.WriteLine("REPROBADO\n");
+            }
+        }
+    }
+
+
+
+    private static int generarNota()
+    {
+        return r.Next(30, 101);
+    }
+    private static void mostrarNombres(List<Alumno> listaAlumnos)
+    {
+        Console.WriteLine("LISTA DE NOMBRES");
+        foreach (var alumno in listaAlumnos)
+        {
+            Console.WriteLine(alumno.Nombre);
+        }
+    }
+}
--- proyectoNotasEstudiantes/Program.cs
+++ proyectoNotasEstudiantes/Program.cs
@@ -4,185 +4,84 @@ class Program
 {
     private static Random r;
     private static int numObjetosRandom;
-
-      static void Main(string[] args)
-
+    static void Main(string[] args)
+    {
+        List<Alumno> listadeAlumnos = new List<Alumno>();
+        obtenerNombresDeLosAlumnos(listadeAlumnos);
+        Console.WriteLine("\nAlumnos registrados:");
+        foreach (var alumno in listadeAlumnos)
         {
-
-            List<Alumno> listadeAlumnos = new List<Alumno>();
-
-            obtenerNombresDeLosAlumnos(listadeAlumnos);
-
-           
-
-            Console.WriteLine("\nAlumnos registrados:");
-
-            foreach (var alumno in listadeAlumnos)
-
-            {
-
-                Console.WriteLine($"- {alumno.Nombre}");
-
-            }
-        
-            generarNotaParaLosAlumnos(listadeAlumnos);
-
-            mostarnotasDelosAlumnos(listadeAlumnos);
-
+            Console.WriteLine($"- {alumno.Nombre}");
         }
-
-       
-
-        public static void crearObjetoRandom()
-
-        {          
-
-            numObjetosRamndom++;
-
-            if(numObjetosRamndom == 1)
-
-                r = new Random();
-
-        }
-
-       
-
-        public static int generarunaNota()
-
-        {
-
-            return r.Next(30,101) ;
-
-        }
-
-       
-
-        static void obtenerNombresDeLosAlumnos(List<Alumno> listadeAlumnos)
-
-        {
-
-            int resultadodeComparacion;
-
-            string nombrealum = string.Empty;
-
-
-            do
-
-            {
-
-                Console.Write("Nombre del alumno (fin para continuar): ");
-
-                nombrealum = Console.ReadLine();
-
-                resultadodeComparacion = string.Compare(nombrealum, "fin");
-
-               
-
-                if(resultadodeComparacion == 0)
-
-                {
-
-                    Console.WriteLine($"Se han introducido {listadeAlumnos.Count} alumnos");
-
-                }
-
-                else if (!string.IsNullOrWhiteSpace(nombrealum))
-
-                {
-
-                    listadeAlumnos.Add(new Alumno(nombrealum));
-
-                }
-
-               
-
-            } while(resultadodeComparacion != 0);
-
-        }
-    
-        static void generarNotaParaLosAlumnos(List<Alumno> listadeAlumnos)
-
-        {
-
-            int suma;
-
-            crearObjetoRandom();
-
-            foreach(Alumno alumno in listadeAlumnos)
-
-            {
-
-                for(int i = 0; i < 5; i++)
-
-                {
-
-                    alumno.ListaNotas.Add(generarunaNota());
-
-                }
-
-                suma = 0;
-
-                foreach (int nota in alumno.ListaNotas)
-
-                {
-
-                    suma += nota;
-
-                }
-
-                alumno.NotaFinal = suma / 4;
-
-            }
-
-        }
-
-       
-
-        static void mostarnotasDelosAlumnos(List<Alumno> listadeAlumnos)
-
-        {
-
-            Console.WriteLine("\n\n--- NOTAS DE LOS ALUMNOS ---");
-
-           
-
-            Console.WriteLine("---------------------------------------------------");
-
-           
-
-            foreach(Alumno alumno in listadeAlumnos)
-
-            {
-
-                Console.Write($"{alumno.Nombre}\t\t");
-
-                Console.Write(string.Join(", ", alumno.ListaNotas));
-
-                Console.Write($"\t\t{alumno.NotaFinal:F1}\t\t");
-
-               
-
-                if(alumno.NotaFinal >= 61)
-
-                {
-
-                    Console.Write("APROBADO");
-
-                }
-
-                else
-
-                {
-
-                    Console.Write("REPROBADO");
-
-                }
-
-                Console.WriteLine();
-
-            }
-
-        }
-
+        generarNotaParaLosAlumnos(listadeAlumnos);
+        mostarnotasDelosAlumnos(listadeAlumnos);
     }
+    public static void crearObjetoRandom()
+    {
+        numObjetosRamndom++;
+        if (numObjetosRamndom == 1)
+            r = new Random();
+    }
+    public static int generarunaNota()
+    {
+        return r.Next(30, 101);
+    }
+    static void obtenerNombresDeLosAlumnos(List<Alumno> listadeAlumnos)
+    {
+        int resultadodeComparacion;
+        string nombrealum = string.Empty;
+        do
+        {
+            Console.Write("Nombre del alumno (fin para continuar): ");
+            nombrealum = Console.ReadLine();
+            resultadodeComparacion = string.Compare(nombrealum, "fin");
+            if (resultadodeComparacion == 0)
+            {
+                Console.WriteLine($"Se han introducido {listadeAlumnos.Count} alumnos");
+            }
+            else if (!string.IsNullOrWhiteSpace(nombrealum))
+            {
+                listadeAlumnos.Add(new Alumno(nombrealum));
+            }
+        } while (resultadodeComparacion != 0);
+    }
+    static void generarNotaParaLosAlumnos(List<Alumno> listadeAlumnos)
+    {
+        int suma;
+        crearObjetoRandom();
+        foreach (Alumno alumno in listadeAlumnos)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                alumno.ListaNotas.Add(generarunaNota());
+            }
+            suma = 0;
+            foreach (int nota in alumno.ListaNotas)
+            {
+                suma += nota;
+            }
+            alumno.NotaFinal = suma / 4;
+        }
+    }
+    static void mostarnotasDelosAlumnos(List<Alumno> listadeAlumnos)
+    {
+        Console.WriteLine("\n\n--- NOTAS DE LOS ALUMNOS ---");
+        Console.WriteLine("---------------------------------------------------");
+        foreach (Alumno alumno in listadeAlumnos)
+        {
+            Console.Write($"{alumno.Nombre}\t\t");
+            Console.Write(string.Join(", ", alumno.ListaNotas));
+            Console.Write($"\t\t{alumno.NotaFinal:F1}\t\t");
+            if (alumno.NotaFinal >= 61)
+            {
+                Console.Write("APROBADO");
+            }
+            else
+            {
+                Console.Write("REPROBADO");
+            }
+            Console.WriteLine();
+        }
+    }
+}
 
